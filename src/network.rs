ae2399a0a2c76789fd1f6eeb1ab779c2eb945ad8
f0lg0 <folgonileonardo@gmail.com>
2021-06-08 18:22:53 +0200

    new: started looking at RPC messages implementation

--- src/network.rs
+++ src/network.rs
@@ -3,33 +3,62 @@ use super::node::*;
 use super::routing::FindValueResult;
 use super::routing::NodeAndDistance;
 
-enum Request {
+use std::collections::HashMap;
+use std::net::UdpSocket;
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::{Arc, Mutex};
+
+pub enum Request {
     Ping,
     Store(String, String),
     FindNode(Key),
     FindValue(String),
 }
 
-enum Response {
+pub enum Response {
     Ping,
     FindNode(Vec<NodeAndDistance>),
     FindValue(FindValueResult),
 }
 
-enum Message {
+pub enum Message {
     Abort,
     Request(Request),
     Response(Response),
 }
 
-struct RpcMessage {
+pub struct RpcMessage {
     token: Key,
     src: Node,
     dst: Node,
     msg: Message,
 }
 
-pub fn ping() {}
-pub fn store() {}
-pub fn find_node() {}
-pub fn find_value() {}
+pub struct Rpc {
+    socket: Arc<UdpSocket>,
+    pending: Arc<Mutex<HashMap<Key, Sender<Option<Response>>>>>,
+    node: Node,
+}
+
+impl Rpc {
+    pub fn new(node: Node, dst: Node) -> Self {
+        let socket = UdpSocket::bind(node.get_addr()).expect(format!(
+            "Rpc::new --> Error while binding UdpSocket to addr {}",
+            node.get_addr()
+        ));
+
+        Self {
+            socket: Arc::new(socket),
+            pending: Arc::new(Mutex::new(HashMap::new())),
+            node,
+        }
+    }
+    pub fn open(rpc: &Rpc) {
+        // TODO: spawn thread and start listening
+    }
+}
+
+// pub fn ping() {}
+// pub fn store() {}
+// pub fn find_node() {}
+// pub fn find_value() {}
