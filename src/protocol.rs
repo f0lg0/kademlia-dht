ce3f9954600b3f580625ce634e6108270af5c56a
f0lg0 <folgonileonardo@gmail.com>
2021-06-09 17:45:59 +0200

    new: rethinking protocol module by hiding implementation logic from the final user
    since this is a library, the final user doesnt need to know about the different functions explained in the paper. Instead, they will get a much simpler API

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,13 +1,30 @@
+use super::network::Rpc;
 use super::node::Node;
+use super::routing::RoutingTable;
 
-pub fn create_node(ip: String, port: u16) -> Node {
-    Node::new(ip, port)
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+#[derive(Debug)]
+pub struct Protocol {
+    routes: Arc<Mutex<RoutingTable>>,
+    store: Arc<Mutex<HashMap<String, String>>>,
+    rpc: Arc<Rpc>,
+    node: Node,
 }
 
-pub fn bootstrap(peer: Node) {}
+impl Protocol {
+    // TODO: missing bootstrap node
+    pub fn new(ip: String, port: u16) -> Self {
+        let node = Node::new(ip, port);
+        let routes = RoutingTable::new(node.clone());
+        let rpc = Rpc::new(node.clone());
 
-pub fn get(key: String) {}
-
-pub fn put(key: String) {}
-
-pub fn join() {}
+        Self {
+            routes: Arc::new(Mutex::new(routes)),
+            store: Arc::new(Mutex::new(HashMap::new())),
+            rpc: Arc::new(rpc),
+            node,
+        }
+    }
+}
