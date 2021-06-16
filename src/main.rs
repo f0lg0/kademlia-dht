b21a1e5adf6ad4bb178613f20a9e18c3514bc479
f0lg0 <folgonileonardo@gmail.com>
2021-06-16 11:10:55 +0200

    edit: better main for debug purposes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 extern crate kademlia_dht;
+use kademlia_dht::node::Node;
 use kademlia_dht::protocol::Protocol;
 use kademlia_dht::utils;
 
 fn main() {
-	let interface0 = Protocol::new(utils::get_local_ip().unwrap(), 1337, None);
-	let interface1 = Protocol::new(utils::get_local_ip().unwrap(), 1338, None);
+	let root = Node::new(utils::get_local_ip().unwrap(), 1337);
+	let interface0 = Protocol::new(root.ip.clone(), root.port.clone(), None);
+	let interface1 = Protocol::new(utils::get_local_ip().unwrap(), 1338, Some(root));
+
+	// utils::dump_interface_state(&interface0, "dumps/interface0.json");
+	// utils::dump_interface_state(&interface1, "dumps/interface1.json");
 
 	interface0.ping(interface1.node.clone());
-	interface1.ping(interface0.node.clone());
+	// interface1.ping(interface0.node.clone());
+
+	utils::dump_interface_state(&interface0, "dumps/interface0.json");
+	utils::dump_interface_state(&interface1, "dumps/interface1.json");
 }
