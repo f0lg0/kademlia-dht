80bba3b2b6ee0f6bec67904a14a446ec2530e36b
f0lg0 <folgonileonardo@gmail.com>
2021-06-25 21:12:50 +0200

    edit: final tests

--- src/lib.rs
+++ src/lib.rs
@@ -29,49 +29,15 @@ const VERBOSE: bool = false;
 #[cfg(test)]
 mod tests {
     use super::key::Distance;
-    use super::key::Key;
     use super::node::Node;
     use super::protocol::Protocol;
     use super::routing::NodeAndDistance;
     use super::utils;
 
-    use std::{thread, time};
-
-    #[test]
-    fn create_key() {
-        let input_str = String::from("test_string");
-        let k = Key::new(input_str);
-        println!("test_string: {:?}", k);
-    }
-    #[test]
-    fn create_node() {
-        let node = Node::new(utils::get_local_ip().unwrap(), 1337);
-        let node_info = node.get_info();
-
-        println!("node: {}", node_info);
-
-        assert_eq!(
-            node_info,
-            "192.168.1.10:1337:B1F14199A00EA18325688FEE9DCD3E48E9269276892C2F3E66135EA15C5C90EB"
-        )
-    }
-
-    #[test]
-    fn distance_between_nodes() {
-        let node0 = Node::new(utils::get_local_ip().unwrap(), 1337);
-        let node1 = Node::new(utils::get_local_ip().unwrap(), 1338);
-
-        let dist = Distance::new(&node0.id, &node1.id);
-        println!(
-            "node0.id: {:?}, node1.id: {:?}, dist: {:?}",
-            node0.id, node1.id, dist
-        );
-    }
-
     #[test]
     fn compare_distance() {
-        let node0 = Node::new(utils::get_local_ip().unwrap(), 1337);
-        let node1 = Node::new(utils::get_local_ip().unwrap(), 1338);
+        let node0 = Node::new(utils::get_local_ip().unwrap(), 1335);
+        let node1 = Node::new(utils::get_local_ip().unwrap(), 1336);
 
         let dist = Distance::new(&node0.id, &node1.id);
         let nd0 = NodeAndDistance(node0.clone(), dist.clone());
@@ -81,25 +47,26 @@ mod tests {
     }
 
     #[test]
-    fn start_protocol() {
-        let root_node = Node::new(utils::get_local_ip().unwrap(), 1339);
+    fn main_test() {
+        let node0 = Node::new(utils::get_local_ip().unwrap(), 1337);
+        let node1 = Node::new(utils::get_local_ip().unwrap(), 1338);
+        let node2 = Node::new(utils::get_local_ip().unwrap(), 1339);
 
-        let interface0 = Protocol::new(utils::get_local_ip().unwrap(), 1339, None);
-        let interface1 = Protocol::new(utils::get_local_ip().unwrap(), 1340, Some(root_node));
+        let interface0 = Protocol::new(node0.ip.clone(), node0.port.clone(), None);
+        let interface1 = Protocol::new(node1.ip.clone(), node1.port.clone(), Some(node0.clone()));
+        let interface2 = Protocol::new(node2.ip.clone(), node2.port.clone(), Some(node0.clone()));
 
-        interface0.ping(interface1.node.clone());
-        interface0.ping(interface1.node.clone());
-        interface1.ping(interface0.node.clone());
+        interface0.put("some_key".to_owned(), "some_value".to_owned());
+        let get_res_1 = interface1.get("some_key".to_owned());
+        let get_res_2 = interface2.get("some_key".to_owned());
 
-        thread::sleep(time::Duration::from_secs(3));
-        utils::dump_interface_state(&interface0, "dumps/interface0.json");
-        utils::dump_interface_state(&interface1, "dumps/interface1.json");
+        assert_eq!("some_value", get_res_1.clone().unwrap());
+        assert_eq!(get_res_1.unwrap(), get_res_2.unwrap());
     }
 
     #[test]
     fn dump_interface() {
-        let interface = Protocol::new(utils::get_local_ip().unwrap(), 1401, None);
+        let interface = Protocol::new(utils::get_local_ip().unwrap(), 1400, None);
         utils::dump_interface_state(&interface, "dumps/interface.json");
-        println!("[*] Dumped interface to file [*]");
     }
 }
